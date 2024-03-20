d6195e11671ef67e59a65528ab333e31af894641
devamloopcon <loopcon1017@gmail.com>
2024-03-20 14:35:57 -0400

    drashti:controller move admin folder, permission add

--- public/database/db_changes.sql
+++ public/database/db_changes.sql
@@ -1,6 +1,6 @@
 -- Disha : 19-03-2024 12:40 PM
 INSERT INTO `roles` (`id`, `name`, `deleted_at`, `created_at`, `updated_at`) VALUES (NULL, 'Super Admin', NULL, NULL, NULL), (NULL, 'Manager', NULL, NULL, NULL), (NULL, 'HR', NULL, NULL, NULL);
-INSERT INTO `modules` (`id`, `module`, `deleted_at`, `created_at`, `updated_at`) VALUES (NULL, 'Home Slider', NULL, NULL, NULL), (NULL, 'Home Our Businesses', NULL, NULL, NULL), (NULL, 'Home Detail', NULL, NULL, NULL), (NULL, 'Testimonials', NULL, NULL, NULL), (NULL, 'Setting', NULL, NULL, NULL);
+INSERT INTO `modules` (`id`, `module`, `deleted_at`, `created_at`, `updated_at`) VALUES (NULL, 'Home Slider', NULL, NULL, NULL), (NULL, 'Home Our Businesses', NULL, NULL, NULL), (NULL, 'Home Detail', NULL, NULL, NULL), (NULL, 'Testimonials', NULL, NULL, NULL), (NULL, 'Setting', NULL, NULL, NULL),(NULL, 'Brand', NULL, NULL, NULL),(NULL, 'Car', NULL, NULL, NULL),(NULL, 'Showroom', NULL, NULL, NULL);
 
 ALTER TABLE `user_role_permission` CHANGE `user_type_id` `role_id` INT(11) NULL DEFAULT NULL COMMENT '1=SuperAdmin,2=Admin,3=HR,4=Manager';
 
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\SettingController;
-use App\Http\Controllers\GalaxyToyotaController;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\BrandController;
-use App\Http\Controllers\CarController;
-use App\Http\Controllers\ShowroomController;
+use App\Http\Controllers\Admin\HomeController;
+use App\Http\Controllers\Admin\SettingController;
+use App\Http\Controllers\Admin\LoginController;
+use App\Http\Controllers\Admin\BrandController;
+use App\Http\Controllers\Admin\CarController;
+use App\Http\Controllers\Admin\ShowroomController;
 
 /*
 |--------------------------------------------------------------------------
@@ -71,6 +70,33 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('setting', [SettingController::class, 'setting'])->name('setting');
     Route::post('setting_insert', [SettingController::class, 'setting_insert'])->name('setting_insert');
 
-   
+    //brand
+    Route::get('brand', [BrandController::class, 'brand'])->name('brand');
+    Route::post('brand_insert', [BrandController::class, 'brand_insert'])->name('brand_insert');
+    Route::get('brand_index', [BrandController::class, 'brand_index'])->name('brand.index');
+    Route::get('brand_edit/{brand_edit}', [BrandController::class, 'brand_edit'])->name('brand.edit');
+    Route::post('brand_update,{brand_update}', [BrandController::class, 'brand_update'])->name('brand_update');
+    Route::delete('brand_destroy/{id}', [BrandController::class, 'brand_destroy']);
+
+    //car
+    Route::get('car', [CarController::class, 'car'])->name('car');
+    Route::post('car_insert', [CarController::class, 'car_insert'])->name('car_insert');
+    Route::get('car_index', [CarController::class, 'car_index'])->name('car.index');
+    Route::get('car_edit/{car_edit}', [CarController::class, 'car_edit'])->name('car.edit');
+    Route::post('car_update,{car_update}', [CarController::class, 'car_update'])->name('car_update');
+    Route::delete('car_destroy/{id}', [CarController::class, 'car_destroy']);
+
+    //showroom
+    Route::get('showroom', [ShowroomController::class, 'showroom'])->name('showroom');
+    Route::post('showroom_insert', [ShowroomController::class, 'showroom_insert'])->name('showroom_insert');
+    Route::get('showroom_index', [ShowroomController::class, 'showroom_index'])->name('showroom.index');
+    Route::get('showroom_edit/{showroom_edit}', [ShowroomController::class, 'showroom_edit'])->name('showroom.edit');
+    Route::post('showroom_update,{showroom_update}', [ShowroomController::class, 'showroom_update'])->name('showroom_update');
+    Route::delete('showroom_destroy/{id}', [ShowroomController::class, 'showroom_destroy']);
+
+    Route::post('facilitie_imagedelete', [ShowroomController::class, 'DeleteFacilitieImage'])->name('facilitie_imagedelete');
+    
+    Route::post('customer_gallery_imagedelete', [ShowroomController::class, 'DeleteCustomerGallery'])->name('customer_gallery_imagedelete');
+
 
 });
