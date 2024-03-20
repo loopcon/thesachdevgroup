29ddfb7b254484b6424aa51126c5d1f675e596f9
Disha <loopcon8@gmail.com>
2024-03-20 14:13:57 +0530

    disha:role base user changes

--- resources/views/admin/role_permission/index.blade.php
+++ resources/views/admin/role_permission/index.blade.php
@@ -7,58 +7,57 @@
                         <div class="col-md-6">
                             <h1 class="m-0">Role Permission</h1>
                         </div>
-
-                        <div class="col-md-3 text-end">
-                            <a href="javascript:void(0);" class="btn btn-info btn-sm" id="check">{{__('Check All')}}</a>&nbsp;&nbsp;&nbsp;&nbsp;
-                            <a href="javascript:void(0);" class="btn btn-danger btn-sm float-right" id="uncheck">{{__('Uncheck All')}}</a>
-                        </div>
                     </div>
                 </div>
             </div>
-            <section class="content">
-                <div class="container-fluid">
-                    <div class="mb-3 col-md-4">
-                        <label class="form-label" for="name">{{__('Role')}}<span class="text-danger">*</span></label>
-                        <select class="form-control" onchange="if (this.value) window.location.href=this.value">
-                            <option value="">-- select --</option>
-                            <option value="{{url('role-permission/'.$constant_superadmin)}}" {{$role_id == $constant_superadmin ? 'selected' : ''}}>{{__('Super Admin')}}</option>
-                            <option value="{{url('role-permission/'.$constant_hr)}}" {{$role_id == $constant_hr ? 'selected' : ''}}>{{__('HR')}}</option>
-                            <option value="{{url('role-permission/'.$constant_manager)}}" {{$role_id == $constant_manager ? 'selected' : ''}}>{{__('MANAGER')}}</option>
-                        </select>
-                    </div>
-                </div>
-            </section>
+            <div class="card">
+                <div class="card-body">
+                    <section class="content">
+                        <div class="container-fluid">
+                            <div class="mb-3 col-md-4">
+                                <label class="form-label" for="name">{{__('Role')}}<span class="text-danger">*</span></label>
+                                <select class="form-control" onchange="if (this.value) window.location.href=this.value">
+                                    <option value="">-- select --</option>
+                                    <option value="{{url('role-permission/'.$constant_superadmin)}}" {{$role_id == $constant_superadmin ? 'selected' : ''}}>{{__('Super Admin')}}</option>
+                                    <option value="{{url('role-permission/'.$constant_hr)}}" {{$role_id == $constant_hr ? 'selected' : ''}}>{{__('HR')}}</option>
+                                    <option value="{{url('role-permission/'.$constant_manager)}}" {{$role_id == $constant_manager ? 'selected' : ''}}>{{__('MANAGER')}}</option>
+                                </select>
+                            </div>
+                        </div>
+                    </section>
 
-            <div class="content-header">
-                <section class="content">
-                    <div class="container-fluid">
-                        @if($role_id)
-                            <form method="post" action="{{route('role-permission')}}" data-parsley-validate>
-                                {{ csrf_field() }}
-                                <input type="hidden" name="role_id" value="<?php echo $role_id; ?>">
-                                @php($modules = getModules())
+                    <div class="content-header">
+                        <section class="content">
+                            <div class="container-fluid">
+                                @if($role_id)
+                                    <form method="post" action="{{route('role-permission')}}" data-parsley-validate>
+                                        {{ csrf_field() }}
+                                        <input type="hidden" name="role_id" value="<?php echo $role_id; ?>">
+                                        @php($modules = getModules())
 
-                                @foreach($modules as $value)
-                                    <div class="row">
-                                        <div class="col-sm-3"><b>{{__($value->module)}}</b></div>
-                                        <div class="col-sm-9">
+                                        @foreach($modules as $value)
                                             <div class="row">
-                                                <div class="col-sm-2">
-                                                    <input type="checkbox" id="read_{{$value->id}}"  name="module_permission[{{$value->id}}][read]" value="1" {{ isset($module_permission[$value->id]['read']) && $module_permission[$value->id]['read'] == 1 ? 'checked' : '' }}><label for="read_{{$value->id}}"> &nbsp;<b> {{ __('Read') }} </b> </label>
-                                                </div>
-                                                <div class="col-sm-2">
-                                                    <input type="checkbox" id="full_{{$value->id}}"  name="module_permission[{{$value->id}}][full]" value="1" {{ isset($module_permission[$value->id]['full']) && $module_permission[$value->id]['full'] == 1 ? 'checked' : '' }}><label for="full_{{$value->id}}"> &nbsp;<b> {{ __('Full') }} </b> </label>
+                                                <div class="col-sm-3"><b>{{__($value->module)}}</b></div>
+                                                <div class="col-sm-9">
+                                                    <div class="row">
+                                                        <div class="col-sm-2">
+                                                            <input type="checkbox" id="read_{{$value->id}}"  name="module_permission[{{$value->id}}][read]" value="1" {{ isset($module_permission[$value->id]['read']) && $module_permission[$value->id]['read'] == 1 ? 'checked' : '' }}><label for="read_{{$value->id}}"> &nbsp;<b> {{ __('Read') }} </b> </label>
+                                                        </div>
+                                                        <div class="col-sm-2">
+                                                            <input type="checkbox" id="full_{{$value->id}}"  name="module_permission[{{$value->id}}][full]" value="1" {{ isset($module_permission[$value->id]['full']) && $module_permission[$value->id]['full'] == 1 ? 'checked' : '' }}><label for="full_{{$value->id}}"> &nbsp;<b> {{ __('Full') }} </b> </label>
+                                                        </div>
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                    </div>
-                                    <hr/>
-                                @endforeach
-                                <button type="submit" class="btn btn-primary">{{__('Submit')}}</button>
-                            </form>
-                        @endif
+                                            <hr/>
+                                        @endforeach
+                                        <button type="submit" class="btn btn-primary">{{__('Submit')}}</button>
+                                    </form>
+                                @endif
+                            </div>
+                        </section>
                     </div>
-                </section>
+                </div>
             </div>
         </div>
     </div>
