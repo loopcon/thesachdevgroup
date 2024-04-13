72380e45f91047a98d328245011c2ab4802caab3
Disha <loopcon8@gmail.com>
2024-04-13 11:38:01 +0530

    disha:hange in sidebar

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -2,7 +2,7 @@
     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
             <li class="nav-item"> 
 
-            <a href="{{url('dashboard')}}" class="nav-link">
+            <a href="{{url('dashboard')}}" class="nav-link {{ request()->is('dashboard') ? 'active' : ''}}">
                 <i class="nav-icon fas fa-tachometer-alt"></i>
                 <p>
                 Dashboard
