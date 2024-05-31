e895bf2bd27a5692ddd33d24a284eb56d3feaacc
Disha <loopcon8@gmail.com>
2024-05-31 15:53:50 +0530

    disha:add db changes

--- app/Http/Controllers/Admin/EmailTemplatesController.php
+++ app/Http/Controllers/Admin/EmailTemplatesController.php
@@ -13,26 +13,48 @@ class EmailTemplatesController extends Controller
 {
     public function emailTemplate()
     {
-        $return_data = array();
-        $return_data['site_title'] = trans('Email Templates');
-        $return_data['email_templates'] = EmailTemplates::orderby('id','desc')->get();
-        return view('admin.email_template.email', array_merge($return_data));
+        $has_permission = hasPermission('Email Templates');
+        if(isset($has_permission) && $has_permission)
+        {
+            if($has_permission->read_permission == 1 || $has_permission->full_permission == 1)
+            {
+                $return_data = array();
+                $return_data['site_title'] = trans('Email Templates');
+                $return_data['email_templates'] = EmailTemplates::orderby('id','desc')->get();
+                return view('admin.email_template.email', array_merge($return_data));
+            }else {
+                return redirect('dashboard')->with('error', trans('You have not permission to access this page!'));
+            }
+        }else {
+            return redirect('dashboard')->with('error', trans('You have not permission to access this page!'));
+        }
     }
 
     public function emailTemplateUpdate(Request $request)
     {
-        $label = $request->id;
-        if($label){
-            $email =  EmailTemplates::select('value')->where('label', $label)->first();
-            $template = isset($email->value) && $email->value ? $email->value : NULL;
+        $has_permission = hasPermission('Email Templates');
+        if(isset($has_permission) && $has_permission)
+        {
+            if($has_permission->read_permission == 1 || $has_permission->full_permission == 1)
+            {
+                $label = $request->id;
+                if($label){
+                    $email =  EmailTemplates::select('value')->where('label', $label)->first();
+                    $template = isset($email->value) && $email->value ? $email->value : NULL;
 
-            if($request->$label) {
-                DB::table('email_templates')
-                    ->where('label', $label)
-                    ->update(['template' => $request->$label]);
+                    if($request->$label) {
+                        DB::table('email_templates')
+                            ->where('label', $label)
+                            ->update(['template' => $request->$label]);
+                    }
+                    return redirect('email-template')->with('success', trans($template.' '. trans('Email Template Updated Successfully!')));
+                }
+                return redirect('email-template')->with('error', trans('Something went wrong, please try again later.'));
+            }else {
+                return redirect('dashboard')->with('error', trans('You have not permission to access this page!'));
             }
-            return redirect('email-template')->with('success', trans($template.' '. trans('Email Template Updated Successfully!')));
+        }else {
+            return redirect('dashboard')->with('error', trans('You have not permission to access this page!'));
         }
-        return redirect('email-template')->with('error', trans('Something went wrong, please try again later.'));
     }
 }
