c38c483b5d56b5830318aabce9b89e2ead2664dd
Disha <loopcon8@gmail.com>
2024-10-21 17:13:56 +0530

    disha:add meta title,meta keyword and meta description in used car

--- resources/views/admin/used_car/form.blade.php
+++ resources/views/admin/used_car/form.blade.php
@@ -550,6 +550,22 @@
                                 @if ($errors->has('number_of_rating')) <div class="text-danger">{{ $errors->first('number_of_rating') }}</div>@endif
                                 <div class="error"></div>
                             </div>
+
+                            <div class="col-md-4 mb-2">
+                                <label for="meta_title">Meta Title</label>
+                                <input type="text" id="meta_title" class="form-control" name="meta_title" value="{{isset($record->meta_title) ? $record->meta_title : old('meta_title')}}">
+                            </div>
+
+                            <div class="col-md-4">
+                                <label for="meta_keyword">Meta Keyword</label>
+                                <textarea class="form-control" name="meta_keyword">{{isset($record->meta_keyword) ? $record->meta_keyword : old('mera_keyword')}}</textarea>
+                            </div>
+
+                            <div class="col-md-4 mb-3">
+                                <label for="meta_description">Meta Description</label>
+                                <textarea class="form-control" name="meta_description">{{isset($record->meta_description) ? $record->meta_description : old('meta_description')}}</textarea>
+                                <div class="error"></div>
+                            </div>
                         </div> 
 
                         <div class="box-footer">
